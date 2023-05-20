cff44a8f0897642ad8028a8ce8cdfc2d2b1e3121
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-05-20 17:00:55 -0400

    Did myself

--- same_optimal.js
+++ same_optimal.js
@@ -11,26 +11,16 @@ same([1,2,1], [4,4,1]) // false (must be same frequency)
 const same = (arr1, arr2) => {
     if (arr1.length !== arr2.length) return false;
 
-    let freqCounter1 = {};
-    let freqCounter2 = {};
+    let counter1 = {};
+    let counter2 = {};
 
-    for (let val of arr1) {
-        freqCounter1[val] = (freqCounter1[val] || 0) + 1;
+    for (let val of arr1) counter1[val] = (counter1[val] || 0) + 1
+    for (let val of arr2) counter2[val] = (counter2[val] || 0) + 1
+
+    for (let key in counter1) {
+        if (counter1[key ** 2] !== counter2[key]) return false;
     }
 
-    for (let val of arr2) {
-        freqCounter2[val] = (freqCounter2[val] || 0) + 1;
-    }
-
-    for (let key in freqCounter1) {
-        if (!(key ** 2 in freqCounter2)) {
-            return false;
-        }
-
-        if (freqCounter2[key ** 2] !== freqCounter1[key]) {
-            return false;
-        }
-    }
     return true;
 };
 
